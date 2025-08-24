d324b840556e26e2863c05462ce8673407c859be
abhimanyutr <abhimanyurajan396@gmail.com>
2025-08-24 11:11:11 +0530

    frontend

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("hakmmclkkdkj ");
+ 
+function getDetails()
+{
+    let name = document.getElementById("recipient-name").value;
+    let phone = document.getElementById("recipient-number").value;
+    let address =document.getElementById("recipient-address").value;
+    let mailId = document.getElementById("recipient-email").value;
+
+    console.log(name);
+    console.log(phone);
+    console.log(address);
+    console.log(mailId);
+}
+
+
+
